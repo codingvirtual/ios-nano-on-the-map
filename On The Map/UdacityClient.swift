9c9c03ee37785935c6b30299221b5fd67d5d77ec
Greg Palen <codingvirtual@gmail.com>
2015-08-05 23:52:36 -0500

    successfully getting Udacity Public User Data

--- On The Map/UdacityClient.swift
+++ On The Map/UdacityClient.swift
@@ -42,7 +42,32 @@ class UdacityClient: NSObject {
                 if let userDict = result.valueForKey("account") as? [String:AnyObject] {
                     UdacityClient.user = UdacityUser(userId: (userDict[JSONResponseKeys.UserID] as? String)!.toInt()!)
                 }
-                completionHandler(result: result, error: nil)
+                UdacityClient.getUserData(UdacityClient.user!, completionHandler: completionHandler)
+            } else {
+                completionHandler(result: nil, error: error)
+            }
+        }
+        /* 7. Start the request */
+        task.resume()
+    }
+    
+    class func getUserData(user: UdacityUser, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        /* 1. Set the parameters */
+        ClientAPILibrary.configure(Constants.BaseURLSecure, baseURLInsecure: Constants.BaseURL)
+        var parameters = ["id": UdacityClient.user!.userId]
+        
+        /* 2/3. Build the URL and configure the request */
+        var method = UdacityClient.Methods.GetUserData
+        
+        
+        /* 4. Build the request */
+        var task = ClientAPILibrary.taskForSecureGETMethod (Methods.GetUserData, parameters: parameters) {result, error in
+            if let jsonResult = result as? NSDictionary {
+                if let userInfo = result.valueForKey("user") as? [String:AnyObject] {
+                    UdacityClient.user!.firstName = userInfo[JSONResponseKeys.FirstName] as? String
+                    UdacityClient.user!.lastName = userInfo[JSONResponseKeys.LastName] as? String
+                }
+                completionHandler(result: UdacityClient.user as? AnyObject, error: nil)
             } else {
                 completionHandler(result: nil, error: error)
             }
