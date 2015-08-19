af8b0a4aedc5850c173ccfecf6d7c130f3b3cdd6
Greg Palen <codingvirtual@gmail.com>
2015-08-19 17:15:12 -0500

    able to show location on GetLinkViewController

--- On The Map/GetLinkViewController.swift
+++ On The Map/GetLinkViewController.swift
@@ -9,14 +9,59 @@
 import Foundation
 import CoreLocation
 import UIKit
+import MapKit
 
-class GetLinkViewController: UIViewController {
+class GetLinkViewController: UIViewController, MKMapViewDelegate  {
     
     var location: CLLocation?
 
+    @IBOutlet weak var linkTF: UITextField!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         println("Location received \(location!.description)")
+        let userLocation = MKPointAnnotation()
+        userLocation.coordinate = location!.coordinate
+        mapView.addAnnotation(userLocation)
+        mapView.centerCoordinate = userLocation.coordinate
     }
    
+    @IBAction func doCancel(sender: AnyObject) {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
+    }
+    
+    @IBAction func doSubmit(sender: AnyObject) {
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinColor = .Red
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(mapView: MKMapView!, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == annotationView.rightCalloutAccessoryView {
+            return
+        }
+    }
 }
