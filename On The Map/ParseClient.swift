13e9bf0a8f67917dd0a3cea4717625ef1de88bd7
Greg Palen <codingvirtual@gmail.com>
2015-08-21 19:11:46 -0500

    add sort order and record limit to Parse getStudentLocations()

--- On The Map/ParseClient.swift
+++ On The Map/ParseClient.swift
@@ -12,7 +12,7 @@ import CoreLocation
 class ParseClient : NSObject {
     
     class func getStudentLocations(completionHandler: ((result: AnyObject?, error: NSError?) -> Void)?) {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?order=-createdAt&limit=100")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = NSURLSession.sharedSession()
